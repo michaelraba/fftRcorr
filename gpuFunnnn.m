7d8441152436a159de45a2c9e31fb20ae9a63086
Michael Raba <michael.raba@gmail.com>
2022-06-01 13:06:54 -0400

    writing classic POD implementation

--- gpuFunnnn.m
+++
@@ -1,4 +0,0 @@
-function [D,E] = gpuFunnnn(A,B,C)
-    D = A.*B + C;
-    E = A+B.*C + 50.45;
-end
--- testTest.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-double x = 100;
-double y = 2;
-double c = x*y;
-    return 0;
-}
