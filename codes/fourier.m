f96ebaabfd7a94d93613a1a129d04f6eeed19aa6
Michael Raba <michael.raba@gmail.com>
2022-06-04 18:05:56 -0400

    filled fourier half vec

--- codes/fourier.m
+++ codes/fourier.m
@@ -1,15 +1,18 @@
 function [fourierVecPost]=fourier(vecPre,aziSet,mSize)
           %deltaTheta = 1/271;
+          fourierVecPost =  zeros(mSize,1);
   deltaTheta = 108*pi/180;
         % calculates the fourier Series
         % NOT using any Dft, although that can be done by using folding - however direct calculation is done here.
-        for mm =mSize
-          a = vecPre(m); % load S(r,r'; k,m) init memory for all r
+  a=zeros(mSize,1);
+        for mm =1:mSize
+          a(mm) = vecPre(mm); % load S(r,r'; k,m) init memory for all r
           %b=zeros(mSize,1);
           b=0;
         for sum_m = mSize
-          b= b +a(mm)*exp(j*sum_m*m)*deltaTheta;
+          b= b +a(mm)*exp(j*sum_m*mm)*deltaTheta;
         end % sum
         fourierVecPost(mm) = b ;
         %  duplicate the data too for anti-aliasing
-        end %m
+        end %mm
+end %fc
